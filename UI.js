bc20162ef63ed040e5143466f78bc47476380f4a
Steven McFarland <54908708+tuh40621@users.noreply.github.com>
2020-11-18 00:42:42 -0500

    Here is some of the backend Files

---
+++ UI.js
@@ -0,0 +1,124 @@
+let data;
+(function getInfo(){
+    let requestURL = "test_data.json";
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        data = request.response;
+        console.log(data["case 1"]["count"]);
+    }
+})()
+
+function featureFunction(num){
+    num = num -1;
+    var modal = document.getElementsByClassName("myModal")[0];
+    // Get the button that opens the modal
+    var btn = document.getElementsByClassName("feature") 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks the button, open the modal 
+    btn[num].onclick = function() {
+    modal.style.display = "block";
+    }
+    injectFunction(num);
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            }
+    }
+}
+function injectFunction(num){
+    var content;
+    var male = 1;
+    var female = 0; 
+    var maleNH=0;
+    var femaleNH=0;
+    var inject = document.getElementsByClassName("txtSection")[0];
+    switch(num){
+        case 0:
+            content = Covid_Gender_inject();
+            
+            break;
+        case 1:
+            content = "THIS IS TEST FEATURE 2";
+            
+            break;
+        case 2:
+            content = "THIS IS TEST FEATURE 3";
+            
+            break;
+        case 3:
+            content = "THIS IS TEST FEATURE 4";
+            
+            break;
+        default:
+            content = "THIS IS A FEATURE";
+            
+    }
+    
+    inject.innerHTML = content;
+    jQuery.ajax({ 
+        type: "GET", 
+        url: "https://phl.carto.com/api/v2/sql?q=SELECT * FROM covid_hospitalizations_by_sex", 
+        dataType: "json", 
+        success: function(data) {
+            console.log(data);
+            var rows = data.rows;
+          
+            rows.forEach(element => {
+                switch (element["cartodb_id"]) {
+                    case 1:
+                        male += element.count;
+                        break;
+                    case 2:
+                        female += element.count;
+                        break;
+                    case 3:
+                        maleNH += element.count;
+                        break;
+                    case 4:
+                        femaleNH += element.count;//NH=Non-Hospitalize
+                        break;
+    
+                    default:
+                        break;
+                }
+            });
+            var ctx=document.getElementById("chartjs-4").getContext("2d")
+            var myChart= new Chart(ctx,
+            {"type":"doughnut",
+            "data":{
+              "labels":[
+                "Male","Female", "Male not Hostpitalized", "Female not Hostpitalized"],
+              "datasets":[{
+                "label":"My First Dataset",
+              "data":[male,female,maleNH,femaleNH],"backgroundColor":[
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 55, 100)",
+              "rgb(54, 45, 235)"]}]}});
+            console.log("male = " + male); 
+            console.log("female = " + female); 
+        }
+    
+    });
+   
+}
+
+function Covid_Gender_inject(){//////////////////Look here to insert the pie chart
+   
+    
+    var content = '<canvas id="chartjs-4" class="chartjs" width="250" height="125" style="display: block; width: 250px; height: 125px;"></canvas>';
+   
+
+    return content;
+};
